de4348ef67f47d47825a7d57ee986940f95be80b
Ansh Pratap <anshpratap866@gmail.com>
2025-10-30 20:16:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
+++ Difficulty: Basic/First n Fibonacci/first-n-fibonacci.java
@@ -1,18 +1,18 @@
 // User function Template for Java
 
 class Solution {
-   
-    public static int[] fibonacciNumbers(int n) {
-        
-                int[] fib = new int[n];
-        fib[0] = 0;
-        if (n > 1) fib[1] = 1;
-
-        for (int i = 2; i < n; i++) {
-            fib[i] = fib[i - 1] + fib[i - 2];
-        }
-        return fib;
+    public static  int fib(int m){
+        if(m == 0 || m == 1) return m;
+        return fib(m-1) + fib(m-2);
+    }
     
+    public static int[] fibonacciNumbers(int n) {
+        int arr[] = new int[n];
+        for(int i = 0; i<n; i++){
+            arr[i] = fib(i);
+        }
+        return arr;
 
+        
     }
 }
