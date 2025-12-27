4a684786907254e9ad01a5eef00ffe11496a7b35
Ansh Pratap <anshpratap866@gmail.com>
2025-12-27 11:29:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Remove Half Nodes/remove-half-nodes.java
@@ -0,0 +1,30 @@
+/*
+
+class Node{
+    int data;
+    Node left,right;
+    Node(int d){
+        data=d;
+        left=right=null;
+    }
+}
+
+*/
+
+class Solution {
+    // Return the root of the modified tree after removing all the half nodes.
+    public Node RemoveHalfNodes(Node root) {
+        if(root == null) return null;
+        root.left = RemoveHalfNodes(root.left);
+        root.right = RemoveHalfNodes(root.right);
+        if(root.left == null && root.right != null){
+            return root.right;
+        }
+        if(root.left != null && root.right == null){
+            return root.left;
+        }
+        
+        return root;
+        
+    }
+}
