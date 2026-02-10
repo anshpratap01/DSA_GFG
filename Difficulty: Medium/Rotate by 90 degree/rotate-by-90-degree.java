2a3c181be0061a1d7114f4a0168a2254ca47117f
Ansh Pratap <anshpratap866@gmail.com>
2026-02-10 21:53:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Rotate by 90 degree/rotate-by-90-degree.java
@@ -0,0 +1,28 @@
+// User function Template for Java
+
+class GFG {
+    static void rotate(int matrix[][]) {
+        // Code Here
+         int n = matrix.length;
+        // transpose
+        for(int i= 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j]  = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        // reverse each row
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<n/2; j++){
+
+                // reverse
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n-1-j];
+                matrix[i][n-1-j]  = temp;
+                
+            }
+        }
+    }
+}
