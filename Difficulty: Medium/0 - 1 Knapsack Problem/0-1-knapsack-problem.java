d743a7f10c7219f29fb9c0688de54b7ae6b49fa8
Ansh Pratap <anshpratap866@gmail.com>
2026-02-19 14:14:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.java
@@ -1,32 +1,30 @@
 class Solution {
     int dp[][];
-    public int knap(int w,int val[], int wt[], int n){
-        if(n == 0 || w == 0) {
-            return 0;
-        }
-        if(dp[n][w] != -1){
-            return dp[n][w];
-        }
-        if(wt[n-1] <= w){
-            int take = val[n-1] +  knap(w-wt[n-1] ,val,wt,n-1);
-            int nottake = knap(w,val,wt,n-1);
-            dp[n][w] = Math.max(take ,nottake);
-        }else{
-        
-            dp[n][w] =   knap(w,val,wt,n-1);
-        }
-        return dp[n][w];
-        
-    }
-    public int knapsack(int W, int val[], int wt[]) {
+    public int knapsack(int w, int val[], int wt[]) {
         int n = val.length;
-         dp = new int[n+1][W+1];
-        for(int i = 0; i<=n;i++){
-            for(int j = 0; j<=W;j++ ){
-                dp[i][j] = -1;
-            }
-        }
-        return knap(W,val,wt,n);
+        dp = new int[n+1][w+1];
+         // base case
+         for(int i = 0; i<=n; i++){
+             for(int j = 0; j<=w; j++){
+                 if(i == 0 || j == 0){
+                     dp[i][j]  = 0;
+                 }
+             }
+         }
+         
+         for(int i = 1; i<=n; i++){
+             for(int j = 1; j<=w;j++){
+                 if(wt[i-1] <= j){
+                     int take = val[i-1] + dp[i-1][j-wt[i-1]];
+                     int notake = dp[i-1][j];
+                     dp[i][j] = Math.max(take , notake);
+                 }else{
+                     dp[i][j] = dp[i-1][j];
+                 }
+             }
+         }
+         return dp[n][w];
+        
         
     }
 }
