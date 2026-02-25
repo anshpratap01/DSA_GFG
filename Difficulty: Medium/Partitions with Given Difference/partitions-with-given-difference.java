c12d73bf218e3848fe569f14d3df9dc5597b28ba
Ansh Pratap <anshpratap866@gmail.com>
2026-02-25 14:23:23 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.java
@@ -0,0 +1,44 @@
+class Solution {
+    int dp[][];
+    public int issubset(int arr[] , int subsetsum , int n){
+        if(n==0){
+            if(subsetsum == 0) return 1;
+            else return 0;
+        }
+        if(dp[n][subsetsum] != -1){
+            return dp[n][subsetsum];
+        }
+        
+        if(arr[n-1] <= subsetsum){
+            int take = issubset(arr,subsetsum-arr[n-1],n-1);
+            int notake = issubset(arr,subsetsum , n-1);
+            dp[n][subsetsum] = take + notake;
+            
+        }else{
+        dp[n][subsetsum]  = issubset(arr,subsetsum ,n-1);
+        }
+        return dp[n][subsetsum];
+    }
+    public int countPartitions(int[] arr, int diff) {
+    
+        // code here
+        int sum = 0;
+        int n = arr.length;
+        
+        for(int i = 0; i<arr.length; i++){
+            sum = sum+arr[i];
+        }
+        
+        int subsetsum = (sum+diff)/2;
+        if((sum+diff)%2 != 0) return 0;
+        dp = new int[n+1][subsetsum+1];
+        for(int i = 0; i<=n; i++){
+            for(int j = 0; j<=subsetsum; j++){
+                dp[i][j] = -1;
+            }
+        }
+        
+        return issubset(arr,subsetsum,n);
+        
+    }
+}
