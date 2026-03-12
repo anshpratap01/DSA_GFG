20a49b6306a77a0e1116a6f2c35d34ab38eb1287
Ansh Pratap <anshpratap866@gmail.com>
2026-03-12 23:00:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -9,21 +9,22 @@ class Solution {
         }
         int min = Integer.MAX_VALUE;
 
-        for(int k = i; k<j; k++){
+        for(int k = i; k<=j-1 ; k++){
           int x = solve(arr, i , k) + solve(arr, k+1, j) + arr[i-1] * arr[k] * arr[j];  
         
            if(x < min){
             min = x;
-            dp[i][j] = min;
+            
            } 
 
         }
+        dp[i][j] = min;
         return dp[i][j];
     }
     static int matrixMultiplication(int arr[]) {
     int i = 1;
     int j = arr.length-1;
-    dp = new int[101][101];
+    dp = new int[arr.length][arr.length];
     for(int ii = 0; ii<dp.length; ii++){
         for(int jj = 0; jj<dp.length; jj++){
             dp[ii][jj] = -1;
